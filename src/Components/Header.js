0558f66a9ae537815f3d25f7c5b0e7e2395a1702
theseventhgod <tobyadeyy@gmail.com>
2023-03-08 19:52:13 +0100

    react

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ import styles from "./header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.headerText}>THE EXCEL (Software Dev.)</h1>
+      <h1 className={styles.headerText}>THE ONLY EXCEL (Software Dev.)</h1>
       <div className={styles.navItems}>
         <a href="#about-me" className={styles.anchor}>
           <i className={`fa-solid fa-user ${styles.icons}`}></i>
